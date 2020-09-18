75f3d92c358264d50e6f8aad4ba2b35fd4f7a5d0
panchao <Hh23232323>
2020-09-18 17:54:59 +0800

    1

--- src/main/java/WaterMark.java
+++ src/main/java/WaterMark.java
@@ -1,5 +1,8 @@
+import com.oracle.webservices.internal.api.databinding.DatabindingMode;
+import lombok.Data;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -15,12 +18,29 @@ public class WaterMark {
         //连接socket获取输入的数据
         DataStreamSource<String> inputStream = env.socketTextStream
                 ("localhost", 6677, "\n");
-        inputStream.map(new MapFunctionction<String, Object>() {
-        })
+        inputStream.map(new MapFunction<String, Object>() {
+            public Object map(String s) throws Exception {
+                String [] arr = s.split(",");
+                return new SensorReading(arr[0], Long.parseLong(arr[1]), Double.parseDouble(arr[2]));
+            }
+        });
 
-
-
+        inputStream.print("output");
 
     }
 
+
+}
+
+@Data
+class SensorReading {
+    public String id;
+    public long ts;
+    public double tp;
+
+    public SensorReading(String _id, long _ts, double _tp) {
+        this.id = _id;
+        this.ts = _ts;
+        this.tp = _tp;
+    }
 }
